e34eba78647910c8eb37a45b3b5439bdceb21978
Eva Houžvičková <houzvickova@icloud.com>
2022-04-04 14:57:17 +0200

    vytvořeno pole otázek pro kvíz

--- kviz.js
+++ kviz.js
@@ -1,6 +1,11 @@
 // Nejdříve si vytvoř objekt, který bude držet voje super otázky :-)
 
-const otazky = [];
+let poleOtazek = [
+    { poradiOtazky: '1/4', obrazek: 'obrazky/moncicak.jpg', otazka:'Které letiště bylo hlavní pražské letiště, než bylo v roce 1937 otevřeno Letiště Ruzyně?', odpoved1: 'Letiště v Letňanech', odpoved2: 'Letiště v Kbelích', odpoved3: 'Letiště ve Strašnicích'}, 
+    { poradiOtazky: '2/4', obrazek: 'obrazky/moncicak.jpg', otazka:'Ve kterém objektu byl pořízen snímek?', odpoved1: 'Čistírna odpadních vod Bubeneč', odpoved2: 'Pivovar Braník', odpoved3: 'Podolská vodárna'},
+    { poradiOtazky: '3/4', obrazek: 'obrazky/moncicak.jpg', otazka:'Víte jak je vysoká velká bronzová socha jezdecká socha Jana Žižky na Vítkově?', odpoved1: '6 metrů', odpoved2: '12 metrů', odpoved3: '9 metrů'},
+    { poradiOtazky: '4/4', obrazek: 'obrazky/moncicak.jpg', otazka:'Poznáte sídliště na obrázku?', odpoved1: 'Černý Most', odpoved2: 'Lužiny', odpoved3: 'Barrandovské'}
+]
 
 // Dále budeš potřebovat další proměnné - jaké?
 
