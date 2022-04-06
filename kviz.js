2907e87f1f356f235dd6447f9007afaf92fd048a
Eva Houžvičková <houzvickova@icloud.com>
2022-04-06 13:04:01 +0200

    propojení funkce KlikNaOdpoved s tlačítky odpovedí

--- kviz.js
+++ kviz.js
@@ -1,9 +1,9 @@
 
 let poleOtazek = [
-    { poradiOtazky: '1/4', obrazek: 'obrazky/moncicak.jpg', otazka:'Které letiště bylo hlavní pražské letiště, než bylo v roce 1937 otevřeno Letiště Ruzyně?', odpoved1: 'Letiště v Letňanech', odpoved2: 'Letiště v Kbelích', odpoved3: 'Letiště ve Strašnicích'}, 
-    { poradiOtazky: '2/4', obrazek: 'obrazky/moncicak.jpg', otazka:'Ve kterém objektu byl pořízen snímek?', odpoved1: 'Čistírna odpadních vod Bubeneč', odpoved2: 'Pivovar Braník', odpoved3: 'Podolská vodárna'},
-    { poradiOtazky: '3/4', obrazek: 'obrazky/moncicak.jpg', otazka:'Víte jak je vysoká velká bronzová socha jezdecká socha Jana Žižky na Vítkově?', odpoved1: '6 metrů', odpoved2: '12 metrů', odpoved3: '9 metrů'},
-    { poradiOtazky: '4/4', obrazek: 'obrazky/moncicak.jpg', otazka:'Poznáte sídliště na obrázku?', odpoved1: 'Černý Most', odpoved2: 'Lužiny', odpoved3: 'Barrandovské'}
+    { poradiOtazky: '1/4', obrazek: 'obrazky/moncicak.jpg', otazka: 'Které letiště bylo hlavní pražské letiště, než bylo v roce 1937 otevřeno Letiště Ruzyně?', odpoved1: 'Letiště v Letňanech', odpoved2: 'Letiště v Kbelích', odpoved3: 'Letiště ve Strašnicích' },
+    { poradiOtazky: '2/4', obrazek: 'obrazky/moncicak.jpg', otazka: 'Ve kterém objektu byl pořízen snímek?', odpoved1: 'Čistírna odpadních vod Bubeneč', odpoved2: 'Pivovar Braník', odpoved3: 'Podolská vodárna' },
+    { poradiOtazky: '3/4', obrazek: 'obrazky/moncicak.jpg', otazka: 'Víte jak je vysoká velká bronzová socha jezdecká socha Jana Žižky na Vítkově?', odpoved1: '6 metrů', odpoved2: '12 metrů', odpoved3: '9 metrů' },
+    { poradiOtazky: '4/4', obrazek: 'obrazky/moncicak.jpg', otazka: 'Poznáte sídliště na obrázku?', odpoved1: 'Černý Most', odpoved2: 'Lužiny', odpoved3: 'Barrandovské' }
 ]
 
 let kviz = document.querySelector('#kviz');
@@ -26,7 +26,6 @@ odpovedi.appendChild(odpoved2);
 odpovedi.appendChild(odpoved3);
 
 let i = 0;
-
 // Zavoláme ji jednou na začátku a poté vždy po odpovězení
 function zobrazOtazku() {
     poradi.innerHTML = poleOtazek[i].poradiOtazky;
@@ -37,11 +36,17 @@ function zobrazOtazku() {
     odpoved3.innerHTML = poleOtazek[i].odpoved3;
 }
 
-// Funkce se postará o obsluhu kliknutí na odpověď
-// Musíme ji navázat na kokrétní odpovědi každé otázky (to uděláme v rámci funkce zobrazOtazku())
-function klikNaOdpoved() {}
+const tlacitkaOdpovedi = document.querySelectorAll('li');
+
+tlacitkaOdpovedi.addEventListener('click', klikNaOdpoved);
+
+
+function klikNaOdpoved() {
+    console.log('test');
+}
+
 
 // Když už mám odpovězeno na vše (řídí se velikosí objektu otazky na řádku 3), tak mohu zobrazi výsledky
 // Vypočítám skóre a nageneruje nové elementy do HTML
 // Touto funkcí končí můj program (budu se rozhodovat, zda ji zavolat v rámci klikNaOdpoved())
-function zobrazVyhodnoceni() {}
+function zobrazVyhodnoceni() { }
